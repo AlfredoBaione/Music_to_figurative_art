557164bc1d8258ab0b45be1325c8749c49200d9a
AlfredoBaione <147424788+AlfredoBaione@users.noreply.github.com>
2025-02-25 11:00:15 +0100

    Update AIS_complete.py

--- AIS_complete.py
+++ AIS_complete.py
@@ -10,7 +10,7 @@ import math
 
 
 # ------------------------------------------------------------------------------
-# Funzione per caricare e processare i file audio
+# Function to upload and process audio
 def get_audio_embeddings(audio_paths, model):
     embeddings_list = []
     for audio_path in audio_paths:
@@ -19,7 +19,7 @@ def get_audio_embeddings(audio_paths, model):
         embeddings_list.append(embeddings)
     return np.array(embeddings_list)
 
-# Funzione per caricare e processare i file immagine
+# Function to upload and process immages
 def get_image_embeddings(image_paths, model_clip, processor):
     embeddings_list = []
     for image_path in image_paths:
@@ -30,39 +30,40 @@ def get_image_embeddings(image_paths, model_clip, processor):
         embeddings_list.append(embeddings.squeeze().numpy())
     return np.array(embeddings_list)
 
-# Ottieni i percorsi dei file nelle directory e ordinali
+# Retrieve the file paths from the directories and sort them
 def get_sorted_file_paths(directory, file_extension):
     file_paths = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith(file_extension)]
     file_paths.sort()  # Ordina i file in base ai loro nomi
     return file_paths
 
 
-# Percorsi alle directory di audio e immagini
+# Paths to the audio and image directories
 audio_directory = 'F:/Music_to_figurative_art/output/performance_measures_test/07/AIS/audio'
 audio_directory_2 = 'F:/audio_test'
 image_directory = 'F:/Music_to_figurative_art/output/performance_measures_test/07/AIS/image'
 
 
-# Ottieni i percorsi ordinati dei file audio e immagine
+# Retrieve the sorted paths of the audio and image files
 audio_paths = get_sorted_file_paths(audio_directory, ".wav")
 image_paths = get_sorted_file_paths(image_directory, ".jpg")
 #ottieni i percorsi dei file audio nella directory di test
 audio_paths_2 = [os.path.join(audio_directory_2, f) for f in os.listdir(audio_directory_2) if f.endswith('.wav')]
 
-# Carica il modello audio wav2clip
+# Load the wav2clip audio model
 audio_model = wav2clip.get_model()
-# Carica il modello CLIP per le immagini
+
+# Load the CLIP model for images
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
-# Ottieni gli embeddings per tutti i file audio
+# Retrieve the embeddings for all audio files
 audio_embeddings = get_audio_embeddings(audio_paths, audio_model)
 audio_embeddings_2 = get_audio_embeddings(audio_paths_2, audio_model)
 # Ottieni gli embeddings per tutte le immagini
 image_embeddings = get_image_embeddings(image_paths, clip_model, clip_processor)
 
 
-# Confronto degli embeddings (ad esempio, il prodotto scalare)
+# Comparison of the embeddings (for example, the dot product)
 def compute_similarity(audio_embeddings, image_embeddings):
     similarities = []
     for audio_emb, image_emb in zip(audio_embeddings, image_embeddings):
@@ -92,15 +93,15 @@ for j in range(len(image_embeddings)):
 
 similarities = (similarities_1 - similarities_2)
 
-# Calcola la media delle similarità
+# Compute the mean similarity
 mean_similarity = np.mean(similarities)
 
-# Mappa la media delle similarità da [-1, 1] a [0, 100] percentuale
+# Mapping the mean similarity from [-1, 1] to [0, 100] (percentage)
 x = math.acos(mean_similarity)
 y = 1 - x * (2 / math.pi)
 mean_similarity_percentage = ((y + 1) / 2) * 100
 
-# Stampa le similarità e la media in percentuale
+# Print the simliarities and the mean percentage
 for i, similarity in enumerate(similarities):
     x_2 = math.acos(similarity)
     y_2 = 1 - x_2 * (2 / math.pi)
@@ -109,6 +110,6 @@ for i, similarity in enumerate(similarities):
 
 print(f"AIS: {mean_similarity_percentage}%")
 
-# Salva gli embeddings per un uso successivo (opzionale)
+# Save the embeddings for later use (optional)
 np.save("audio_embeddings.npy", audio_embeddings)
 np.save("image_embeddings.npy", image_embeddings)
